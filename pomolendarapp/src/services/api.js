a005900b15842389e5124ccd1e65cdced00992a1
Tuan Hoang <tuanhoang5050@gmail.com>
2026-09-03 19:39:43 +0700

    prep beta

--- pomolendarapp/src/services/api.js
+++ pomolendarapp/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://192.168.1.18:8000/api'; 
+const BASE_URL = 'http://192.168.1.8:8000/api'; 
 
 const api = axios.create({
   baseURL: BASE_URL,
